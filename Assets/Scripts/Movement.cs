009fa6ac73bb7a015794ebe47f6c0a4d38a8dc47
Anri-Laz <anril@freightpop.com>
2026-02-10 23:44:07 +0400

    Initial Unity Rocket game project

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+using UnityEditor.Callbacks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction thrust;
+    [SerializeField] float thrustStrength = 150f;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        thrust.Enable();       
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+}
