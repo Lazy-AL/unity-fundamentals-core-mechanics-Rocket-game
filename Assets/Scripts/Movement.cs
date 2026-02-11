3209d8a270a813a65728e8e7525a8c36074aaea7
Anri-Laz <anril@freightpop.com>
2026-02-11 23:52:23 +0400

    controls

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,9 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
-    [SerializeField] float thrustStrength = 150f;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStrength = 900f;
+    [SerializeField] float rotationStrength = 80f;
     Rigidbody rb;
 
     private void Start()
@@ -15,14 +17,39 @@ public class Movement : MonoBehaviour
 
     private void OnEnable()
     {
-        thrust.Enable();       
+        thrust.Enable();
+        rotation.Enable();    
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() 
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+    private void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+
+        if(rotationInput < 0)
+        {
+            AplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            AplyRotation(-rotationStrength);
+        }
+    }
+
+    private void AplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
     }
 }
