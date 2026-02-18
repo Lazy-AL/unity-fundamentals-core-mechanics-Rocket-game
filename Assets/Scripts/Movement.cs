937ab5e8e7cead13da5cd5f5d03c414a414ac580
Anri-Laz <anri.lazashvilib@gmail.com>
2026-02-18 23:48:27 +0400

    audio

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -48,10 +48,10 @@ public class Movement : MonoBehaviour
         }
     }
 
-    private void AplyRotation(float rotationThisFrame)
-    {
-        rb.freezeRotation = true;
-        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
-        rb.freezeRotation = false;
-    }
+    // private void AplyRotation(float rotationThisFrame)
+    // {
+    //     rb.freezeRotation = true;
+    //     transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+    //     rb.freezeRotation = false;
+    // }
 }
