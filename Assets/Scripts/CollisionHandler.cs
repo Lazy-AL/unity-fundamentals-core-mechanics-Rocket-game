6fe72a938e0f8c2a064cc993430c3984f27cc78b
Anri-Laz <anri.lazashvilib@gmail.com>
2026-02-27 23:42:25 +0400

    fix bug

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -8,36 +8,35 @@ public class CollisionHandler : MonoBehaviour
         switch (other.gameObject.tag)
         {
             case "Friendly":
-    //             Debug.Log("All good");
-    //             break;
+                Debug.Log("All good");
+                break;
 
-    //         case "Finish":
-    //             LoadNextLevel();
-    //             break;
+            case "Finish":
+                LoadNextLevel();
+                break;
 
-    //         default:
-    //             Invoke("ReloadLevel", 2f);
-    //             break;
-    //     }
+            default:
+                Invoke("ReloadLevel", 2f);
+                break;
+        }
+    }
 
-    // void ReloadLevel()
-    // {
-    //     int currentScene = SceneManager.GetActiveScene().buildIndex;
-    //     SceneManager.LoadScene(currentScene);
-    // }
+    void ReloadLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
+    }
 
     void LoadNextLevel()
     {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         int nextScene = currentScene + 1;
 
-        if(nextScene == SceneManager.sceneCountInBuildSettings)
-            {
-                nextScene = 0;
-            }
+        if (nextScene == SceneManager.sceneCountInBuildSettings)
+        {
+            nextScene = 0;
+        }
 
         SceneManager.LoadScene(nextScene);
-    }
-
     }
 }
